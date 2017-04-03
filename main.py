d386489f333a41aa60d2f6e8887320c197c17d34
ilias <suleimanov121@gmail.com>
2017-04-03 17:38:26 +0300

    Server opens 'index.html', if it contains in the current directory. And if not - form the html with message.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import socket
+import os
 
 HOST, PORT = '', 8008
 MSG_LEN = 1024
@@ -7,13 +8,30 @@ MSG_LEN = 1024
 s = socket.socket(socket.AF_INET)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
-s.listen(0)
+s.listen(5)
 
 while True:
     client_connection, client_address = s.accept()
     request = client_connection.recv(MSG_LEN)
     print(request)
-    response_bytes = str.encode('<h1>Hello</h1>')
 
-    client_connection.sendall(response_bytes)
+    response_data = ''
+    if os.path.isfile('index.html'):
+        with open('index.html') as f:
+            response_data = str.encode(f.read())
+    else:
+        response_data = str.encode('''
+            <!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Title</title>
+            </head>
+            <body>
+                <h2>Hi! I didn't find the "index.html" on the server, sorry;(</h2>
+            </body>
+            </html>
+        ''')
+
+    client_connection.sendall(response_data)
     client_connection.close()
